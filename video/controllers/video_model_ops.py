5577f5de6dd1758cfcc7e1eadb3803e19ec01916
Chayan Datta <chayan.datta@decentro.tech>
2021-05-29 02:16:50 +0530

    pagination

--- video/controllers/video_model_ops.py
+++ video/controllers/video_model_ops.py
@@ -8,8 +8,12 @@ class VideoModelOps:
 
     @staticmethod
     def get_based_on_search(search_str: str) -> [Video]:
-        return Video.objects.annotate(
-            search=SearchVector('title', 'description'),
-         ).filter(
-            search=search_str
-        )
+        # return Video.objects.annotate(
+        #     search=SearchVector('title', 'description'),
+        #  ).filter(
+        #     search=search_str
+        # )
+        return Video.objects.filter(
+            Q(title__icontains=search_str) |
+            Q(description__icontains=search_str)
+        ).order_by('-published_at')
--- video/views.py
+++ video/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from video.controllers.video_model_ops import VideoModelOps
@@ -19,19 +20,17 @@ class VideoSearchView(GenericAPIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        videos = VideoModelOps().get_based_on_search(search_string)
-        if not videos:
+        self.queryset = VideoModelOps().get_based_on_search(search_string)
+        if not self.queryset:
             return Response(
                 data={'error': f'No videos found for [{search_string}]'},
                 status=status.HTTP_404_NOT_FOUND
             )
 
-        paginated_qs = self.paginate_queryset(videos)
-        serialized_videos = self.serializer_class(videos, many=True)
+        paginator = PageNumberPagination()
+        paginator.page_size = 50
 
-        return Response(
-            data={
-                'count': len(paginated_qs),
-                'videos': serialized_videos.data
-            }
-        )
+        paginated_queryset = paginator.paginate_queryset(self.queryset, request)
+        serialized_videos = self.serializer_class(paginated_queryset, many=True)
+
+        return paginator.get_paginated_response(serialized_videos.data)
